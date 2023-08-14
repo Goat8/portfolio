c0dcec8107c1c950bd15dfc943a08705698770bc
Goat8 <mariarafique12@gmail.com>
2023-08-14 14:55:30 +0500

    fix: lint error

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center ">
       <h1 className="text-6xl font-bold">Hi, </h1>
-      <h1 className="text-6xl font-bold">I'm Maria, </h1>
+      <h1 className="text-6xl font-bold">I&apos;m Maria,</h1>
       <h1 className="text-6xl font-bold">Full Stack developer </h1>
 
 
       <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full ">
         <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
           <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <div className="font-bold text-xl mb-2">NodeJS</div>
             <p className="text-gray-700 text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatibus quia, nulla! Maiores et perferendis eaque,
@@ -20,13 +20,13 @@ export default function Home() {
           </div>
           <div className="px-6 pt-4 pb-2">
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #photography
+              #authentication
             </span>
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #travel
+              #restApi
             </span>
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #winter
+              #middleware
             </span>
           </div>
         </div>
